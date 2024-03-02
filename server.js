2147f30d50b5e71cae223ed505244498a0353fc1
lisavanmansom <lisavanmansom@gmail.com>
2024-03-02 22:37:00 +0100

    laatste versie

--- server.js
+++ server.js
@@ -16,21 +16,57 @@ const app = express()
 // Stel ejs in als template engine
 app.set('view engine', 'ejs')
 
+// Zorg dat werken met request data makkelijker wordt 
+app.use(express.urlencoded({extended: true}))
+
 // Stel de map met ejs templates in
 app.set('views', './views')
 
 // Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
 app.use(express.static('public'))
 
-// Maak een GET route voor de index, defineer urls
+
+// Maak een GET route voor de index
 app.get('/', function (request, response) {
   // Haal alle personen uit de WHOIS API op
-  fetchJson(apiUrl + '/person/?filter={"squad_id":4}&sort=name').then((apiData) => {
+  var query = request.query;
+
+  var url = apiUrl + '/person';
+
+  if (query.name || query.id) {
+    var queryString = '?filter={';
+    var filters = [];
+
+    if (query.name) {
+      // Zet de naam eerst volledig naar kleine letters en vervolgens de eerste letter naar hoofdletter
+      var formattedName = query.name.toLowerCase().charAt(0).toUpperCase() + query.name.toLowerCase().slice(1);
+      filters.push('"name":"' + encodeURIComponent(formattedName) + '"');
+    }
+
+    if (query.id) {
+      filters.push('"squad_id":"' + encodeURIComponent(query.id) + '"');
+    }
+
+    queryString += filters.join(', ') + '}';
+
+    url += queryString;
+  } else {
+    // Maak url alleen voor squad_id 4
+    var queryString = '?filter={';
+    var filters = [];
+    filters.push('"squad_id":"' + 4 + '"');
+
+    queryString += filters.join(', ') + '}';
+
+    url += queryString;
+  }
+
+  fetchJson(url).then((apiData) => {
     // apiData bevat gegevens van alle personen uit alle squads
     // Je zou dat hier kunnen filteren, sorteren, of zelfs aanpassen, voordat je het doorgeeft aan de view
 
     // Render index.ejs uit de views map en geef de opgehaalde data mee als variabele, genaamd persons
-    response.render('index', {persons: apiData.data, squads: squadData.data})
+    response.render('index', { persons: apiData.data, squads: squadData.data })
   })
 })
 
@@ -45,9 +81,41 @@ app.get('/person/:id', function (request, response) {
   // Gebruik de request parameter id en haal de juiste persoon uit de WHOIS API op
   fetchJson(apiUrl + '/person/' + request.params.id).then((apiData) => {
     // Render person.ejs uit de views map en geef de opgehaalde data mee als variable, genaamd person
-    response.render('person', {person: apiData.data, squads: squadData.data})
+    response.render('person', { person: apiData.data, squads: squadData.data })
   })
 })
+
+app.post('/:id/like-or-unlike', function (request, response) {
+  // STAP 1: Haal de huidige gegevens voor deze persoon op, uit de WHOIS API
+  fetchJson('https://fdnd.directus.app/items/person/' + request.params.id).then((apiResponse) => { // Het custom field is een String, dus die moeten we eerst omzetten (= parsen)
+    // naar een Object, zodat we er mee kunnen werken
+    try {
+      apiResponse.data.custom = JSON.parse(apiResponse.data.custom)
+    } catch (e) {
+      apiResponse.data.custom = {}
+    }
+    // STAP 2: Voeg de like toe aan het custom object..
+    if (request.body.action == 'liked') {
+      apiResponse.data.custom.like = true
+    } else {
+      apiResponse.data.custom.like = false
+    }
+    // STAP 3: Overschrijf het custom field voor deze persoon
+    fetchJson('https://fdnd.directus.app/items/person/' + request.params.id, {
+      method: 'PATCH',
+      body: JSON.stringify({
+        custom: apiResponse.data.custom
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      }
+    }).then((patchResponse) => {
+      // Redirect naar de persoon pagina
+      response.redirect(303, '/#members')
+    })
+  })
+})
+
 
 // Stel het poortnummer in waar express op moet gaan luisteren
 app.set('port', process.env.PORT || 8000)
